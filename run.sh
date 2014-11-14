afa84cd679777540d75c53b436b7b8c5a37b6042
Antonio Molina <antoniomolina13@gmail.com>
2014-11-13 21:09:48 -0500

    Fixed exports of variables in run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ export PATH=/usr/local/bin:$PATH
 
 forever stop main.js
 
-OUTLOG=./logs/node-log.txt
-ERRLOG=./logs/node-error-log.txt
-FOREVERLOG=./logs/forever-log.txt
-HOSTNAME=map.amolina.ca
-PORT=7003
+export OUTLOG=./logs/node-log.txt
+export ERRLOG=./logs/node-error-log.txt
+export FOREVERLOG=./logs/forever-log.txt
+export HOSTNAME=map.amolina.ca
+export PORT=7003
 
 forever --minUptime 5000 --spinSleepTime 2000 -a -o $OUTLOG -e $ERRLOG start main.js >> $FOREVERLOG 2>&1
